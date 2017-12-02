241d92311a147757194ff12fc6b210bba1fe2081
Scott Mitchell <mitchsco@sonoma.edu>
2017-12-01 22:32:48 -0800

    Added improved permissions to use profile feed items API

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -9,6 +9,7 @@ router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset') # Non model view set
 router.register('profile',views.UserProfileViewSet) # Only for model view sets
 router.register('login', views.LoginViewSet, base_name='login')
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
 	url(r'^hello-view/', views.HelloApiView.as_view()),
